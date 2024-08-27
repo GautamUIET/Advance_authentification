1f87e12e04a0a88813bffda0d80befaa60f04d8a
GautamUIET <gautamjain09687@gmail.com>
2024-08-27 23:07:25 +0530

    2

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand'
 import axios from 'axios';
 import ForgotPassword from '../Pages/ForgotPassword';
-const API_URL = "http://localhost:5000/api/auth"
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set)=>({
